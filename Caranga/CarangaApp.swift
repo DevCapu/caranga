e11937754c4a6427102a50c85e113c4c45e7e51a
devcapu <felipe.b2014@gmail.com>
2023-10-25 19:27:19 -0300

    Initial Commit

---
+++ Caranga/CarangaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarangaApp.swift
+//  Caranga
+//
+//  Created by Felipe Moreno Borges on 25/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CarangaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
